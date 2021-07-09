63515ad038fba784132f5e1ef9a655ffbb639880
dshaw1242 <david.shaw1242@gmail.com>
2021-07-08 20:20:14 -0600

    created reaction subdocument in thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        reaction: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody:{
+            type: String,
+            required: 'reaction text is required',
+            validate: [({ length }) => length <= 280, 'text must be between 1 and 280 characters']
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+    }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
 
 const ThoughtSchema = new Schema(
     {
@@ -28,6 +57,10 @@ const ThoughtSchema = new Schema(
         }
     }
 );
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
 
 const Thought = model('Thought', ThoughtSchema);
 
